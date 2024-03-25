a2db48b4f201048ef30de2bb39daffe649d89acd
saulotarsobc <saulotarsobc@gmail.com>
2024-03-25 18:08:42 -0300

    :fire:

--- js/app.js
+++ js/app.js
@@ -15,17 +15,6 @@ if (dataParam !== null) {
       ZOOM_DEFALT
     );
 
-    /* para usar o mapbox */
-    // L.tileLayer(
-    //   "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-    //   {
-    //     attribution: '&copy; <a href="https://www.mapbox.com/">Mapbox</a>',
-    //     id: "mapbox/streets-v12",
-    //     accessToken:
-    //       "pk.eyJ1IjoibWFwYm94LW1hcC1kZXNpZ24iLCJhIjoiY2syeHpiaHlrMDJvODNidDR5azU5NWcwdiJ9.x0uSqSWGXdoFKuHZC5Eo_Q",
-    //   }
-    // ).addTo(map);
-
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
       // maxZoom: 19,
       attribution:
@@ -53,30 +42,3 @@ if (dataParam !== null) {
 } else {
   console.log('O parâmetro "data" não foi encontrado na URL.');
 }
-
-// var popup = L.popup();
-
-// function onMapClick(e) {
-//     popup
-//     .setLatLng(e.latlng)
-//     .setContent("You clicked the map at " + e.latlng.toString())
-//     .openOn(map);
-// }
-
-// map.on("click", onMapClick);
-
-// var circle = L.circle([51.508, -0.11], {
-//     color: 'red',
-//     fillColor: '#f03',
-//     fillOpacity: 0.5,
-//     radius: 500
-// }).addTo(map);
-
-// var polygon = L.polygon([
-//     [51.509, -0.08],
-//     [51.503, -0.06],
-//     [51.51, -0.047]
-// ]).addTo(map);
-
-// circle.bindPopup("I am a circle.");
-// polygon.bindPopup("I am a polygon.");
